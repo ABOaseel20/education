1b4cdef2375b1a9459299b3e74f2a87a291a5431
ABOaseel20 <134551320+ABOaseel20@users.noreply.github.com>
2024-08-17 15:36:55 +0300

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('home.css')
 <body>
 
 
 <div class="container mb-2 mt-2">
 
         
-@include('home.nav')
-
-
-
-@include('home.content')
-@include('home.footer')
+</div>
 
 </body>
 </html>
